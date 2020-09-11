90637cb9087e0ed30354e8037643249475a72d50
movaclub <seeusers@gmail.com>
2020-09-11 18:13:49 +0300

    finished

---
+++ README.md
@@ -0,0 +1,13 @@
+## CSV processor
+
+### Application overview
+
+![Application screen](https://github.com/movaclub/js-cvs/blob/master/screenshot.png)
+
+To start using the application one has to pick a CSV file (_no headers please_); as soon as the CSV file touches the form, it gets parsed and processed: left-hand side reflects the source file content, while on the right, there's a processed, i.e. having zero's interpolated variant of the matrix.
+
+### Tools used
+
+* JavaScript
+* HTML
+* CSS
--- js/index.js
+++ js/index.js
@@ -1,20 +1,71 @@
 // js entry point
 
-function procFile(file) {
-  if( !file ) return;
+function procFile(file) { // read in src csv file and output it
+  if (!file) return;
   const srcCSV = [];
   const reader = new FileReader();
   reader.addEventListener('load', (e) => {
     srcCSV.push(e.target.result);
   });
   reader.addEventListener('loadend', (ev => {
-    console.log('srcCSV: ', srcCSV);
     let rows = [];
-    srcCSV.forEach( el => {
-      rows = el.split(/[\r\n]+/);
+    srcCSV.forEach(el => {
+      rows = el.split(/[\r\n]+/).filter(el => el.length);
     });
-    const txtSrc = document.querySelector('#txt-src').innerHTML = '<code>' +
-      rows.join('<br>') + '</code>';
+    document.querySelector('#txt-src').innerHTML = '<code>' + rows.join('<br>') + '</code>';
+    procSCV(rows);
   }));
   reader.readAsText(file);
+}
+
+function procSCV(rows) { // process csv data, locate zeros, interpolate them, output result set
+  let matrix = []; // source set
+  let interpolated = []; // resulting set
+  rows.forEach(row => {
+    matrix.push(row.split(',').map(el => parseInt(el)))
+  });
+  for (let r = 0; r < matrix.length; r++) {
+    for (let c = 0; c < matrix[r].length; c++) {
+      if (typeof interpolated[r] === "undefined") {
+        interpolated[r] = [];
+      }
+      if (matrix[r][c] === 0) {
+        interpolated[r][c] = ifZero({r: r, c: c}, matrix);
+      } else {
+        interpolated[r][c] = matrix[r][c];
+      }
+    }
+  }
+  const markupStrings = interpolated.map( el => el.join(','));
+  document.querySelector('#txt-proc').innerHTML = '<code>' + markupStrings.join('<br>') + '</code>';
+}
+
+function ifZero(idx, matrix) { // calculate replacement for zero
+  const {r, c} = idx;
+  // assume we've got all 8 neighbors, let'em be like these
+  let ul = 0, ut = 0, ur = 0;
+  let sl = 0, sr = 0;
+  let bl = 0, bb = 0, br = 0;
+
+  // NB: if a neighbor equals to zero we count it anyway
+  ul = (matrix[r - 1] && matrix[r - 1][c - 1]) ? parseInt(matrix[r - 1][c - 1]) : undefined;
+  ut = (matrix[r - 1] && matrix[r - 1][c])     ? parseInt(matrix[r - 1][c])     : undefined;
+  ur = (matrix[r - 1] && matrix[r - 1][c + 1]) ? parseInt(matrix[r - 1][c + 1]) : undefined;
+
+  bl = (matrix[r + 1] && matrix[r + 1][c - 1]) ? parseInt(matrix[r + 1][c - 1]) : undefined;
+  bb = (matrix[r + 1] && matrix[r + 1][c])     ? parseInt(matrix[r + 1][c])     : undefined;
+  br = (matrix[r + 1] && matrix[r + 1][c + 1]) ? parseInt(matrix[r + 1][c + 1]) : undefined;
+
+  sl = (matrix[r] && matrix[r][c - 1]) ? parseInt(matrix[r][c - 1]) : undefined;
+  sr = (matrix[r] && matrix[r][c + 1]) ? parseInt(matrix[r][c + 1]) : undefined;
+
+  let counter = 0; //  arithmetic mean divisor
+  let sum = 0; // arithmetic sum
+  [ul, ut, ur, sl, sr, bl, bb, br].forEach((count) => {
+    if (count) {
+      sum += count;
+      counter += 1;
+    }
+  });
+  return Math.round(sum / counter);
 }
